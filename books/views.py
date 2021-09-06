85ae7b51a030beb61857af2f69233a960893a7b6
fandemisterling100 <mi.jaramillo@uniandes.edu.co>
2021-09-06 11:50:15 -0500

    New app created and index of the app added

--- books/views.py
+++ books/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    
+    return render(request, "books/index.html", {
+        "message": None
+    })
+
+def search(request):
+    
+    title = request.POST["title"]
+    author = request.POST["author"]
+    print(title)
+    print(author)
+    if (title is None or title == "") and (author is None or author == ""):
+        return render(request, "books/index.html", {
+        "message": "You should enter an author or title value to start your search"
+    })
+    
+    return HttpResponse("Funciona! {} {}".format(title, author))
