c5f790c6ac5050741ac302e8d9e0df5d5eb2dd7e
fandemisterling100 <mi.jaramillo@uniandes.edu.co>
2021-09-06 12:58:55 -0500

    Html and css for results page

--- books/views.py
+++ books/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .utilities import * 
 
 def index(request):
+    """Render the index page to start the search"""
     
     return render(request, "books/index.html", {
         "message": None
     })
 
 def search(request):
+    """Search view to receive the values of author and title to 
+    call the API"""
     
+    # Get author and title
     title = request.POST["title"]
     author = request.POST["author"]
-    print(title)
-    print(author)
+
+    # Server-side validation of the form to get at least one value
     if (title is None or title == "") and (author is None or author == ""):
         return render(request, "books/index.html", {
         "message": "You should enter an author or title value to start your search"
     })
     
-    return HttpResponse("Funciona! {} {}".format(title, author))
+    # Call API to get results
+    books = [{
+        "title": "Lord of the Rings",
+        "author": "J. R. R. Tolkien",
+        "publish_date": "October 2001",
+        "publisher": "Coles Editorial Board",
+        "isbn": "0774011181",
+        "subject": "Motion pictures"
+        }, {
+        "title": "Harry Potter and the Philosopher's Stone",
+        "author": "J.K. Rowling",
+        "publish_date": "1999 November 25",
+        "publisher": "Scholastic Canada",
+        "isbn": "9780747546290",
+        "subject": "Fantasy fiction"
+        }]
+    
+    # Render results
+    
+    
+    return render(request, 'books/results.html', {
+        "books": books
+    })
