8cab4b3120ffa2b524c96222a24f8c37ed892191
fandemisterling100 <mi.jaramillo@uniandes.edu.co>
2021-09-06 21:17:00 -0500

    Code organized according to PEP8 standard

--- books/services.py
+++ books/services.py
@@ -1,69 +1,114 @@
 """Service layer to make the request to the corresponding
-EndPoint at  https://openlibrary.org/developers/api."""
+EndPoint at  https://openlibrary.org/developers/api
+"""
 
 
 import requests
-from requests.models import Response
 
+# URL to make requests to the API
 URL = "http://openlibrary.org/search.json"
+
+# Maximum limit of books to show
 SEARCH_LIMIT = 10
 
-def create_books_dictionary(response):
-    
+
+def create_books_dictionaries(response):
+    """ Function to create a list of dictionaries
+        with the information of title, author, ISBN,
+        first year of publication, publisher and subject
+        of each result.
+
+        Inputs: response (json)
+        Outputs: books (list)
+    """
+
+    # Select the subset of SEARCH_LIMIT books
     docs = response.get("docs")[:SEARCH_LIMIT]
-    #import pdb; pdb.set_trace()
     books = []
-    
+
+    # Iterate over the results to get basic info
+    # from each book
     for result in docs:
-        
-        author = result.get("author_name")
-        author = author[0] if author else "No Author"
-        subject = result.get("subject")
-        subject = ", ".join(subject) if subject else "No Subject"
-        isbn = result.get("isbn")
-        isbn = isbn[0] if isbn else "No ISBN"
-        
-        books.append(
-            {
-                "title": result.get("title"),
-                "author": author,
-                "isbn": isbn,
-                "publish_date": result.get("first_publish_year"),
-                "publisher": "Publisher",
-                "subject": subject
-            }
-        )
-        
+
+        try:
+            # Validations of fields for each result
+            author = result.get("author_name")
+            author = author[0] if author else "No Author"
+            subject = result.get("subject")
+            subject = ", ".join(subject) if subject else "No Subject"
+            isbn = result.get("isbn")
+            isbn = isbn[0] if isbn else "No ISBN"
+            publisher = result.get("publisher")
+            publisher = publisher[0] if publisher else "No publisher"
+
+            # Build the dictionary with basic information
+            books.append(
+                {
+                    "title": result.get("title"),
+                    "author": author,
+                    "isbn": isbn,
+                    "publish_date": result.get("first_publish_year"),
+                    "publisher": publisher,
+                    "subject": subject
+                }
+            )
+        except:
+            books = None
+
     return books
 
-def response_handler(response):
-    
-    if response.status_code == 200:
 
-        return create_books_dictionary(response.json())
+def response_handler(response):
+    """Function to know the status of the request
+        to the API according to the parameters
+        provided by the user
+
+        Inputs: response (requests.models.Response)
+        Outputs: 0 (int) if the result was not successful, a (list) otherwise
+    """
+
+    if response.status_code == 200:
+        return create_books_dictionaries(response.json())
     else:
         return 0
 
+
 def get_books(url, params):
-    
+    """ Function to make the request to the API
+
+        Inputs: url (str), params (dict)
+        Outputs: response (requests.models.Response)
+    """
+
     response = requests.get(url, params=params)
     return response_handler(response)
 
 
 def get_books_handler(title, author):
-    
+    """Function to build the parameter dictionary
+        according to the values provided by the user
+        to make the request.
+
+        Inputs: title (str), author (str), or None if they
+                were not provided.
+        Outputs: 0 (int) if the result was not successful, a (list) otherwise
+    """
+
+    # Validation of existing parameters
     no_title = title is None or title == ""
     no_author = author is None or author == ""
-    
+
+    # Build the parameter dictionary
     if no_title:
         params = {"author": author}
-        
-    elif no_author: 
+
+    elif no_author:
         params = {"title": title}
-        
+
     else:
         params = {"author": author, "title": title}
-        
+
+    # Get the books according to the parameters
     books = get_books(URL, params)
-     
+
     return books
--- books/views.py
+++ books/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .services import get_books_handler
 
+
 def index(request):
-    """Render the index page to start the search"""
-    
+    """Render the index page to start the search
+    """
+
     return render(request, "books/index.html", {
         "message": None
     })
 
+
 def search(request):
-    """Search view to receive the values of author and title to 
-    call the API"""
-    
+    """Search view to receive the values of author and title to
+    call the API
+    """
+
     # Get author and title
     title = request.POST.get("title")
     author = request.POST.get("author")
@@ -22,18 +26,24 @@ def search(request):
         return render(request, "books/index.html", {
         "message": "You should enter an author or title value to start your search"
     })
-    
+
     # Call API to get results
     books = get_books_handler(title, author)
-    
+
     # Validate Response
-    if books:
-        
+    if books and books != 0:
+
         # Render results
         return render(request, 'books/results.html', {
             "books": books
         })
-    else:
+    elif books == 0:
+        # Error in the request
+        return render(request, 'books/results.html', {
+            "books": None,
+            "message": "We couldn't process your request, please try again."
+        })
+    elif not books:
         # Results not found
         return render(request, 'books/results.html', {
             "books": books,
